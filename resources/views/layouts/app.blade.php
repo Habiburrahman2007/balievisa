64cdbaef7aed526073c3d4d15d99530543c61788
Muhammad Habiburrahman Al Arif <habibmas2007@gmail.com>
2026-04-22 11:37:26 +0700

    bismillah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,7 +96,7 @@
 
   {{-- ─── Font Awesome (non-blocking) ─────────────────── --}}
   <script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"
-    integrity="sha512-GWzVrcGlo0adTR7780BBcxPaq587eeLoLQW9Wao5UJvC79jTHNH969ny140CtZVYkbWPVK8nONuQujiqhF76iA=="
+    integrity="sha512-GWzVrcGlo0TxTRvz9ttioyYJ+Wwk9Ck0G81D+eO63BaqHaJ3YZX9wuqjwgfcV/MrB2PhaVX9DkYVhbFpStnqpQ=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <style>
     /* Prevent icon flash (FA SVG approach) */
